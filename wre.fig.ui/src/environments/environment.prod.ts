8c558586a315f427228edca7ac7fc2b435343da0
Vishal Nagwan <vishal.nagwan@drivenbrands.com>
2026-05-20 16:43:14 -0400

    Fix API URL to use correct Azure App Service hostname

--- wre.fig.ui/src/environments/environment.prod.ts
+++ wre.fig.ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:     'https://WreFigAPIService.azurewebsites.net/api',
-  hubUrl:     'https://WreFigAPIService.azurewebsites.net/hubs/fig'
+  apiUrl:     'https://wrefigapiservice-cchcarbjevhhhxfb.eastus2-01.azurewebsites.net/api',
+  hubUrl:     'https://wrefigapiservice-cchcarbjevhhhxfb.eastus2-01.azurewebsites.net/hubs/fig'
 };
